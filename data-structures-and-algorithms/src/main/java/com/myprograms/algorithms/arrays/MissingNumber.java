a954274e2cea29ce9a17d9f8e2239df2978e1af7
Martel Richard <martelmr36@gmail.com>
2020-01-09 08:19:26 +0530

    Missing number updated

--- data-structures-and-algorithms/src/main/java/com/myprograms/algorithms/arrays/MissingNumber.java
+++ data-structures-and-algorithms/src/main/java/com/myprograms/algorithms/arrays/MissingNumber.java
@@ -3,13 +3,13 @@ package com.myprograms.algorithms.arrays;
 import java.util.Arrays;
 
 public class MissingNumber {
-    public int findMissingNumber(int[] arr) {
+    public static int findMissingNumber(int[] arr) {
         int n = arr.length;
         final int total = (n + 1) * (n + 2) / 2;
         return total - Arrays.stream(arr).sum();
     }
 
-    public int findMissingNumberUsingXOR(int[] arr) {
+    public static int findMissingNumberUsingXOR(int[] arr) {
         int n = arr.length;
         int totalExpected = 0, totalActual = 0;
         for (int i = 1; i <= n + 1; i++) {
@@ -21,6 +21,31 @@ public class MissingNumber {
         }
 
         return totalExpected ^ totalActual;
+    }
 
+    public static int[] findTwoMissingNumbers(int[] arr) {
+        int size = arr.length + 2;
+
+        long totalSum = size * (size + 1) / 2;
+
+        long arrSum = 0;
+        for (int i : arr)
+            arrSum += i;
+
+        int pivot = (int) ((totalSum - arrSum) / 2);
+
+        int totalLeftXor = 0;
+        int arrLeftXor = 0;
+        int totalRightXor = 0;
+        int arrRightXor = 0;
+
+        for (int i = 1; i <= pivot; i++) totalLeftXor ^= i;
+        for (int i = pivot + 1; i <= size; i++) totalRightXor ^= i;
+        for (int i : arr) {
+            if (i <= pivot) arrLeftXor ^= i;
+            else arrRightXor ^= i;
+        }
+
+        return new int[]{totalLeftXor ^ arrLeftXor, totalRightXor ^ arrRightXor};
     }
 }
--- data-structures-and-algorithms/src/test/java/com/myprograms/algorithms/arrays/MissingNumberTest.java
+++ data-structures-and-algorithms/src/test/java/com/myprograms/algorithms/arrays/MissingNumberTest.java
@@ -2,6 +2,7 @@ package com.myprograms.algorithms.arrays;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class MissingNumberTest {
@@ -10,13 +11,20 @@ public class MissingNumberTest {
     public void findMissingNumber() {
         int[] arr = {2, 1, 7, 8, 5, 3, 4, 9};
         int missing = 6;
-        assertEquals("Cannot find missing number", missing, new MissingNumber().findMissingNumber(arr));
+        assertEquals("Cannot find missing number", missing, MissingNumber.findMissingNumber(arr));
     }
 
     @Test
     public void findMissingNumberUsingXOR() {
         int[] arr = {2, 1, 7, 8, 5, 3, 4, 9};
         int missing = 6;
-        assertEquals("Cannot find missing number", missing, new MissingNumber().findMissingNumberUsingXOR(arr));
+        assertEquals("Cannot find missing number", missing, MissingNumber.findMissingNumberUsingXOR(arr));
+    }
+
+    @Test
+    public void findTwoMissingNumbers() {
+        int[] arr = {2, 1, 7, 8, 5, 4, 9};
+        int[] missing = {3, 6};
+        assertArrayEquals("Cannot find missing number", missing, MissingNumber.findTwoMissingNumbers(arr));
     }
 }
